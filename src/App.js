77d0d401e0959fef60097aaca5a78199f0e518f1
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-01 19:29:40 -0300

    begin the project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> NEGO NEY NEGO NEY NEGO NEY.
+          Edit <code>src/App.js</code> NEasdfasdfsdf
         </p>
         <a
           className="App-link"
